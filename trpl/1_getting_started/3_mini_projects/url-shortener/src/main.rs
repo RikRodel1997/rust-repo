19bd96a603e9d979751dbd27cd1d78c949fc4775
RikRodel1997 <145914563+RikRodel1997@users.noreply.github.com>
2024-03-04 09:08:53 +0100

    Continued on url shortener

--- trpl/1_getting_started/3_mini_projects/url-shortener/src/main.rs
+++ trpl/1_getting_started/3_mini_projects/url-shortener/src/main.rs
@@ -9,13 +9,15 @@ use rand::{self, seq::SliceRandom};
 
 fn main() {
     let mut urls: Vec<Url> = Vec::new();
+
     println!("Would you like to generate some testdata (Y/N)? Shortened URLs will be generated for youtube.com and google.com.");
     let mut testdata_choice: String = String::new();
     io::stdin().read_line(&mut testdata_choice).expect("Couldn't get your input, sorry.");
-    match testdata_choice.trim() {
+
+    match testdata_choice.trim().to_uppercase().as_str() {
         "Y" => {
             println!("Generating testdata...");
-            generate_testdata(urls)
+            urls = generate_testdata(urls);
         },
         "N" => {
             println!("No testdata will be generated!");
@@ -25,11 +27,46 @@ fn main() {
             main();
         }
     }
+
+    menu(urls)
+
+
+
+}
+
+fn menu(urls: Vec<Url>) {
+    let mut next_step: String = String::new();
+    println!("What would you like to do next?");
+    println!("1. Open a url.\n2. Add a new url.\n3. Remove an url.");
+    io::stdin().read_line(&mut next_step).expect("Couldn't determine your next step, sorry.");
+
+    match next_step.trim() {
+        "1" => {
+            open_url(&urls);
+        },
+        "2" => {
+
+        },
+        "3" => {
+
+        },
+        _ => {
+
+        }
+    }
+}
+
+fn open_url(urls: &Vec<Url>) {
+    for (n, url) in urls.iter().enumerate() {
+        let formatted_id = format!("{:2}", n + 1);
+        println!("{} | {} {}", formatted_id, url.website_name, url.short_code);
+    } 
 }
 
 fn generate_testdata(mut urls: Vec<Url>) -> Vec<Url> {
     let youtube_short_url = Url {
         id: 1,
+        website_name: String::from("YouTube"),
         original_url: String::from("https://youtube.com"),
         short_code: generate_short_code()
     };
@@ -38,34 +75,33 @@ fn generate_testdata(mut urls: Vec<Url>) -> Vec<Url> {
 
     let google_short_url: Url;
     let mut google_short_code = generate_short_code();
-    let mut is_new_short_code = check_if_short_code_already_exists(urls, google_short_code);
+    let mut is_new_short_code = check_if_short_code_already_exists(&urls, google_short_code);
 
     while !is_new_short_code {
         google_short_code = generate_short_code();
-        is_new_short_code = check_if_short_code_already_exists(urls, google_short_code);
+        is_new_short_code = check_if_short_code_already_exists(&urls, google_short_code);
     }
 
     google_short_url = Url {
         id: 2,
+        website_name: String::from("Google"),
         original_url: String::from("https://google.com"),
         short_code: generate_short_code()
     };
 
     urls.push(google_short_url);
+    println!("{:?}", urls);
     urls
-
     
-}
+} 
 
 // Potentially put this as an impl of Url
-fn check_if_short_code_already_exists(urls: Vec<Url>, short_code: String) -> bool {
+fn check_if_short_code_already_exists(urls: &Vec<Url>, short_code: String) -> bool {
     if urls.len() > 0 {
         for url in urls.iter() {
             if short_code == url.short_code {
                 println!("Found duplicate short_code.");
                 return false;
-            } else {
-                return true;
             }
         }
     }
@@ -82,11 +118,6 @@ fn generate_short_code() -> String {
         }
     }
 
-    let short_code: String = characters.choose_multiple(&mut rand::thread_rng(), 8).collect();
-
-    short_code
+    let short_code: String = characters.choose_multiple(&mut rand::thread_rng(), 8).collect()    short_code
 }
 
-fn open_url() {
-
-}
--- trpl/1_getting_started/3_mini_projects/url-shortener/src/module.rs
+++ trpl/1_getting_started/3_mini_projects/url-shortener/src/module.rs
@@ -1,5 +1,7 @@
+#[derive(Debug)]
 pub struct Url {
     pub id: i32,
+    pub website_name: String,
     pub original_url: String,
     pub short_code: String
 }
