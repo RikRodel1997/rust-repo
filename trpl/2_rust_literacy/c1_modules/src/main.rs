fb0aaaedbafdb9ccd6e67bfa8bab1fe95cf27aca
RikRodel1997 <145914563+RikRodel1997@users.noreply.github.com>
2024-03-18 20:52:46 +0100

    Finished part 2 c1

--- trpl/2_rust_literacy/c1_modules/src/main.rs
+++ trpl/2_rust_literacy/c1_modules/src/main.rs
@@ -1,3 +1,13 @@
+pub mod a {
+    pub mod series {
+        pub mod of {
+            pub fn nested_modules() {}
+        }
+    }
+}
+
+use a::series::of;
+
 fn main() {
-    println!("Hello, world!");
+    of::nested_modules()
 }
