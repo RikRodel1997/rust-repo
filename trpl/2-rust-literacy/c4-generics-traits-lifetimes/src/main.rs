f064a2f415839563004e2d9ab2ec548d4dd5520a
RikRodel1997 <145914563+RikRodel1997@users.noreply.github.com>
2024-05-18 16:10:50 +0200

    Finished c4.3

--- trpl/2-rust-literacy/c4-generics-traits-lifetimes/src/main.rs
+++ trpl/2-rust-literacy/c4-generics-traits-lifetimes/src/main.rs
@@ -1,20 +1,41 @@
+use structs::*;
+use std::cmp::PartialOrd;
+
+mod structs;
+
 fn main() {
-    c4_3();
+    c4_2();
 }
 
 fn c4_3() {
-    
+    let tweet = Tweet {
+        username: "horse_ebooks".to_string(),
+        content: "of course, as you probably know, people".to_string(),
+        reply: false,
+        retweet: false
+    };
+    println!("1 new tweet: {}", tweet.summary());
+
+    let article = NewsArticle {
+        headline: "Penguins win the Stanley Cup Championship".to_string(),
+        location: "Pittsburg, PA, USA".to_string(),
+        author: "Iceburgh".to_string(),
+        content: "The Pittsburg Penguins once again are the best hockey team in the NHL.".to_string(),
+    };
+
+    notify(tweet);
+    notify(article);
 }
 
 #[allow(dead_code)]
 fn c4_1() {
     let numbers = vec![34, 50, 25, 100, 65];
-    let result = largest_i32(&numbers);
+    let result = largest(&numbers);
     println!("The largest number is {result}");
     assert_eq!(result, 100);
 
     let numbers = vec![102, 34, 6_000, 89, 54, 2, 43, 8];
-    let result = largest_i32(&numbers);
+    let result = largest(&numbers);
 
     println!("The largest number is {result}");
     assert_eq!(result, 6_000);
@@ -23,46 +44,28 @@ fn c4_1() {
 #[allow(dead_code)]
 fn c4_2() {
     let numbers = vec![34, 50, 25, 100, 65];
-    let result = largest_i32(&numbers);
+    let result = largest(&numbers);
     println!("The largest number is {result}");
     assert_eq!(result, 100);
 
     let chars = vec!['y', 'm', 'a', 'q'];
-    let result = largest_char(&chars);
+    let result = largest(&chars);
 
     println!("The largest number is {result}");
     assert_eq!(result, 'y');
 }
 
-fn largest_i32(list: &[i32]) -> i32 {
+fn notify<T: Summarizable>(item: T) {
+    println!("Breaking news! {}", item.summary());
+}
+
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
     let mut largest = list[0];
     for &item in list.iter() {
         if item > largest {
             largest = item;
         }
     }
-    
+
     largest
 }
-
-fn largest_char(list: &[char]) -> char {
-    let mut largest = list[0];
-    for &item in list.iter() {
-        if item > largest {
-            largest = item;
-        }
-    }
-    
-    largest
-}
-
-// fn largest_t<T>(list: &[T]) -> T {
-//     let mut largest = list[0];
-//     for &item in list.iter() {
-//         if item > largest {
-//             largest = item;
-//         }
-//     }
-
-//     largest
-// }
---
+++ trpl/2-rust-literacy/c4-generics-traits-lifetimes/src/structs.rs
@@ -0,0 +1,28 @@
+pub trait Summarizable {
+
+    fn summary(&self) -> String {
+        String::from("(Read more...)")
+    }
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summarizable for NewsArticle {}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summarizable for Tweet {
+    fn summary(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
