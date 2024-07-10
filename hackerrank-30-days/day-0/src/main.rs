a562c20323028629ce3ef8923bb45b805202710f
RikRodel1997 <145914563+RikRodel1997@users.noreply.github.com>
2024-07-10 18:28:45 +0200

    Removed rust from hackerrank-30-days

--- hackerrank-30-days/day-0/src/main.rs
+++
@@ -1,9 +0,0 @@
-use std::io;
-
-fn main() {
-    let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Unable to read user input");
-    println!("Hello, World.\n{}", input);
-}
