a66ea9ea013b52f4b174dfd7dc44423f2e957320
RikRodel1997 <origin1997bf4@gmail.com>
2024-10-06 16:13:19 +0200

    Rustlings 45/94
    Rustlings exercises 44 & 45

--- rustlings/exercises/11_hashmaps/hashmaps3.rs
+++ rustlings/exercises/11_hashmaps/hashmaps3.rs
@@ -31,6 +31,31 @@ fn build_scores_table(results: &str) -> HashMap<&str, TeamScores> {
         // Keep in mind that goals scored by team 1 will be the number of goals
         // conceded by team 2. Similarly, goals scored by team 2 will be the
         // number of goals conceded by team 1.
+        if !scores.contains_key(team_1_name) {
+            scores.insert(team_1_name, TeamScores {goals_scored: team_1_score, goals_conceded: team_2_score});
+        } else {
+            let existing_scores = scores.get(team_1_name).unwrap();
+            let new_scores = TeamScores {
+                goals_scored: existing_scores.goals_scored + team_1_score,
+                goals_conceded: existing_scores.goals_conceded + team_2_score,
+            };
+            scores.insert(team_1_name, new_scores);
+
+        }
+
+        if !scores.contains_key(team_2_name) {
+            scores.insert(team_2_name, TeamScores {goals_scored: team_2_score, goals_conceded: team_1_score});
+        } else {
+            let existing_scores = scores.get(team_2_name).unwrap();
+            let new_scores = TeamScores {
+                goals_scored: existing_scores.goals_scored + team_2_score,
+                goals_conceded: existing_scores.goals_conceded + team_1_score,
+            };
+            scores.insert(team_2_name, new_scores);
+        }
+
+
+
     }
 
     scores
