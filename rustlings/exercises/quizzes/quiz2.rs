177f2e719623416e724bcbd1018108a56a1514b2
RikRodel1997 <origin1997bf4@gmail.com>
2024-10-10 21:50:25 +0200

    Rustlings 48/94

--- rustlings/exercises/quizzes/quiz2.rs
+++ rustlings/exercises/quizzes/quiz2.rs
@@ -27,7 +27,24 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> { 
+        let mut output = Vec::new();
+        for entry in input.iter() {
+            let transformed = match entry.1 {
+                Command::Uppercase => entry.0.to_uppercase(),
+                Command::Trim => entry.0.trim().to_string(),
+                Command::Append(times) => {
+                    let mut result = entry.0.clone();
+                    for _ in 0..times {
+                        result.push_str("bar");
+                    }
+                    result
+                }
+            };
+            output.push(transformed);
+        }
+        output
+    }
 }
 
 fn main() {
@@ -37,7 +54,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+    use crate::my_module::transformer;
     use super::Command;
 
     #[test]
